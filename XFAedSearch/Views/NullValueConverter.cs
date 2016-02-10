ae989438f16f82baaea315270bf0c547d05f5adf
P3PPP <ticktackmob@gmail.com>
2016-02-11 02:11:26 +0900

    色とレイアウトを調整

---
+++ XFAedSearch/Views/NullValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFAedSearch.Views
+{
+	public class NullValueConverter : IValueConverter
+	{
+		public Object SubstituteValue
+		{
+			get;
+			set;
+		}
+
+		#region IValueConverter implementation
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return value ?? SubstituteValue;
+
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+		public NullValueConverter()
+		{
+			SubstituteValue = "shokichi";
+		}
+	}
+}
+
