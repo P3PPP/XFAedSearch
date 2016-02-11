bc2a84d4f1a11ef7a929b8afec3aa56a628026c6
P3PPP <ticktackmob@gmail.com>
2016-02-11 20:47:54 +0900

    グローバルエラーハンドラを追加

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,6 +7,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace XFAedSearch.Droid
 {
@@ -17,12 +20,78 @@ namespace XFAedSearch.Droid
 		{
 			base.OnCreate(bundle);
 
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+			TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;  
+
 			global::Xamarin.Forms.Forms.Init(this, bundle);
 			global::Xamarin.FormsMaps.Init(this, bundle);
 			NControl.Droid.NControlViewRenderer.Init();
+			DisplayCrashReport();
 
 			LoadApplication(new App());
 		}
+
+		private static void TaskSchedulerOnUnobservedTaskException(object sender,
+			UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs)
+		{
+			var newExc = new Exception("TaskSchedulerOnUnobservedTaskException",
+				unobservedTaskExceptionEventArgs.Exception);
+			LogUnhandledException(newExc);
+		}  
+
+		private static void CurrentDomainOnUnhandledException(object sender,
+			UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+		{
+			var newExc = new Exception("CurrentDomainOnUnhandledException",
+				unhandledExceptionEventArgs.ExceptionObject as Exception);
+			LogUnhandledException(newExc);
+		}  
+
+		internal static void LogUnhandledException(Exception exception)
+		{
+			try
+			{
+				const string errorFileName = "Fatal.log";
+				var libraryPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+				var errorFilePath = Path.Combine(libraryPath, errorFileName);  
+				var errorMessage = String.Format("Time: {0}\r\nError: Unhandled Exception\r\n{1}",
+					DateTime.Now, exception.ToString());
+				File.WriteAllText(errorFilePath, errorMessage);  
+
+				System.Diagnostics.Debug.WriteLine("Crash Report", errorMessage);
+			}
+			catch
+			{
+				// 何もできない
+			}
+		}
+
+		[Conditional("DEBUG")]
+		private void DisplayCrashReport()
+		{
+			const string errorFilename = "Fatal.log";
+			var libraryPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+			var errorFilePath = Path.Combine(libraryPath, errorFilename);
+
+			if (!File.Exists(errorFilePath))
+			{
+				return; 
+			}
+
+			var errorText = File.ReadAllText(errorFilePath);
+			new AlertDialog.Builder(this)
+				.SetPositiveButton("Clear", (sender, args) =>
+				{
+					File.Delete(errorFilePath);
+				})
+				.SetNegativeButton("Close", (sender, args) =>
+				{
+					// User pressed Close.
+				})
+				.SetMessage(errorText)
+				.SetTitle("Crash Report")
+				.Show();
+		} 
 	}
 }
 
--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -4,6 +4,9 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace XFAedSearch.iOS
 {
@@ -12,6 +15,11 @@ namespace XFAedSearch.iOS
 	{
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+			TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+
+			DisplayCrashReport();
+
 			global::Xamarin.Forms.Forms.Init();
 			global::Xamarin.FormsMaps.Init();
 			NControl.iOS.NControlViewRenderer.Init();
@@ -19,6 +27,65 @@ namespace XFAedSearch.iOS
 			LoadApplication(new App());
 
 			return base.FinishedLaunching(app, options);
+		}
+
+		private static void TaskSchedulerOnUnobservedTaskException(object sender,
+			UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs)
+		{
+			var newExc = new Exception("TaskSchedulerOnUnobservedTaskException",
+				unobservedTaskExceptionEventArgs.Exception);
+			LogUnhandledException(newExc);
+		}  
+
+		private static void CurrentDomainOnUnhandledException(object sender,
+			UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+		{
+			var newExc = new Exception("CurrentDomainOnUnhandledException",
+				unhandledExceptionEventArgs.ExceptionObject as Exception);
+			LogUnhandledException(newExc);
+		}  
+
+		internal static void LogUnhandledException(Exception exception)
+		{
+			try
+			{
+				const string errorFileName = "Fatal.log";
+				var libraryPath = Environment.GetFolderPath(Environment.SpecialFolder.Resources);
+				var errorFilePath = Path.Combine(libraryPath, errorFileName);  
+				var errorMessage = String.Format("Time: {0}\r\nError: Unhandled Exception\r\n{1}",
+					DateTime.Now, exception.ToString());
+				File.WriteAllText(errorFilePath, errorMessage);  
+
+				System.Diagnostics.Debug.WriteLine("Crash Report", errorMessage);
+			}
+			catch
+			{
+				// 何もできない
+			}
+		}  
+
+		[Conditional("DEBUG")]
+		private static void DisplayCrashReport()
+		{
+			const string errorFilename = "Fatal.log";
+			var libraryPath = Environment.GetFolderPath(Environment.SpecialFolder.Resources);
+			var errorFilePath = Path.Combine(libraryPath, errorFilename);
+
+			if (!File.Exists(errorFilePath))
+			{
+				return;
+			}
+
+			var errorText = File.ReadAllText(errorFilePath);
+			var alertView = new UIAlertView("Crash Report", errorText, null, "Close", "Clear") { UserInteractionEnabled = true };
+			alertView.Clicked += (sender, args) =>
+			{
+				if (args.ButtonIndex != 0)
+				{
+					File.Delete(errorFilePath);
+				}
+			};
+			alertView.Show();
 		}
 	}
 }
